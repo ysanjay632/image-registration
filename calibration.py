cc000914df7b34b8b58ef1030f6157def85c92d5
Sanjay <ysanjay632@gmail.com>
2023-06-14 10:01:27 +0530

    calibration.py

--- calibration.py
+++ calibration.py
@@ -1,77 +1,79 @@
-# this method is purely well tested program 
-
 import sunpy.map
 import glob
 import numpy as np
-from aiapy.calibrate import register
-from sunpy.io  import write_file
+import aiapy.data.sample as sample_data
+from aiapy.calibrate import  register
+from sunpy.io import write_file
 import sunkit_image.coalignment as c
 import astropy.units as u
+
 import matplotlib.pyplot as plt
 from sunpy.image import resample
-filelist= glob.glob("file_path/*.fits")
-
-map= [sunpy.map.Map(f) for f in filelist]
-maps = [[] for _ in range(len(map))]
-smap = [[] for i in range(len(map))]
-for i in range(len(map)):
-    smap[i]= register(map[i],missing=0,order =3,method='scipy')
 
 
-    smap[i].meta['naxis1'] = 4096
-    smap[i].meta['naxis2'] = 4096
+def align_and_register(file_path):
+    filelist = glob.glob(file_path + "/*.fits")
+    map_list = [sunpy.map.Map(f) for f in filelist]
+    maps = [[] for _ in range(len(map_list))]
+    smap = [[] for _ in range(len(map_list))]
 
-    width_diff = 4096 - smap[i].data.shape[0]
-    height_diff = 4096 - smap[i].data.shape[1]
-    crpix1 = smap[i].meta['crpix1'] + width_diff / 2
-    crpix2 = smap[i].meta['crpix2'] + height_diff / 2
+    for i in range(len(map_list)):
+        smap[i] = register(map_list[i], missing=0, order=3, method='scipy')
+        smap[i].meta['naxis1'] = 4096
+        smap[i].meta['naxis2'] = 4096
 
-    smap[i].meta['crpix1'] = crpix1
-    smap[i].meta['crpix2'] = crpix2
-    sm = resample.resample(smap[i].data, [4096, 4096])
-    maps[i] = sunpy.map.Map(sm, smap[i].meta)
-    print(i)
+        width_diff = 4096 - smap[i].data.shape[0]
+        height_diff = 4096 - smap[i].data.shape[1]
+        crpix1 = smap[i].meta['crpix1'] + width_diff / 2
+        crpix2 = smap[i].meta['crpix2'] + height_diff / 2
 
-data = [[] for _ in range(len(maps))]
-data2 = [[] for _ in range(len(maps))]
-yshift=[[] for _ in range(len(maps))]
-xshift =[[] for _ in range(len(maps))]
-y=[[] for _ in range(len(maps))]
-x =[[] for _ in range(len(maps))]
-sy=[[] for _ in range(len(maps))]
-sx =[[] for _ in range(len(maps))]
+        smap[i].meta['crpix1'] = crpix1
+        smap[i].meta['crpix2'] = crpix2
 
-for i in range(len(maps)):
-    data[i] = ((maps[i].data))
-    
-temp = data[0][2200:3000,2000:3000]   # can be chosen different
-print(np.max(temp))
-for i in range(len(maps)):
-    shift = c.calculate_shift(data[i],temp)
-    print(shift[1],shift[0])
-    yshift[i] = shift[0]
-    xshift[i]= shift[1]
+        sm = resample.resample(smap[i].data, [4096, 4096])
+        maps[i] = sunpy.map.Map(sm, smap[i].meta)
+        print(i)
 
+    map_sequence = sunpy.map.MapSequence(maps)
+    data = [[] for _ in range(len(maps))]
+  
+    yshift = [[] for _ in range(len(maps))]
+    xshift = [[] for _ in range(len(maps))]
+    y = [[] for _ in range(len(maps))]
+    x = [[] for _ in range(len(maps))]
+    sy = [[] for _ in range(len(maps))]
+    sx = [[] for _ in range(len(maps))]
 
-for i in range(len(maps)):
-    sx[i] = xshift[0]-xshift[i]
+    for i in range(len(maps)):
+        data[i] = ((maps[i].data))
+       
 
-    sy[i] = yshift[0] - yshift[i]
+    temp = data[0][2200:3000, 2000:3000]  # can be chosen differently
+    print(np.max(temp))
 
-#print(xshift,yshift)
-y = u.Quantity(sy, unit=u.pix)
-x = u.Quantity(sx, unit=u.pix)
-print(x,y)
-aligned = c.apply_shifts(mapss,y,x,clip= False)
-for i in range(len(aligned)):
-    print(aligned[i].data.shape)
-    print(maps[i].data.shape)
-for i in range(len(maps)):
-    
-    t = aligned[i].meta['DATE-OBS']
-    t = t.replace(':', '_')
-    file = f'AIA_1600_{t}.fits'        # can be chosen different
+    for i in range(len(maps)):
+        shift = c.calculate_shift(data[i], temp)
+        print(shift[1], shift[0])
+        yshift[i] = shift[0]
+        xshift[i] = shift[1]
 
-    write_file(file, aligned[i].data, aligned[i].meta, filetype= 'fits')
+    for i in range(len(maps)):
+        sx[i] = xshift[0] - xshift[i]
+        sy[i] = yshift[0] - yshift[i]
 
+    y = u.Quantity(sy, unit=u.pix)
+    x = u.Quantity(sx, unit=u.pix)
+    print(x, y)
+    aligned = c.apply_shifts(map_sequence, y, x, clip=False)
 
+    for i in range(len(aligned)):
+        print(aligned[i].data.shape)
+        print(maps[i].data.shape)
+
+    for i in range(len(maps)):
+        t = aligned[i].meta['DATE-OBS']
+        t = t.replace(':', '_')
+        file = f'AIA_1700_{t}.fits'
+
+        write_file(file, aligned[i].data, aligned[i].meta, filetype= 'fits')
+align_and_register('C:/Users/ysanj/sunpy/data/1700')
